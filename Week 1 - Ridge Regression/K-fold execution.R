aabeebdfa441ffa06012fed7dd2fed2cc9b5f2ef
Merlijn-van-Breugel <merlijnvanbreugel@gmail.com>
2016-09-05 15:30:53 +0200

    blabla

--- Week 1 - Ridge Regression/K-fold execution.R
+++ Week 1 - Ridge Regression/K-fold execution.R
@@ -1,4 +1,3 @@
-
 # Title:    Week 1 Ridge Regression - K-fold execution
 # Course:   Multivariate Statistics
 # Authors:  Merlijn van Breugel
@@ -10,6 +9,9 @@ data<- supermarket1996[6:50]                        #X
 data<- cbind(supermarket1996["GROCERY_sum"],data)   #append y to X
 
 data<- data[sample(nrow(data)),]                    #Shuffle data
+#Add column with fold-categories
+fold<- c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5)
+data<- cbind(data,fold)
 #Choose Matrices X and y
 y<- data[,1]
 X<- data[,c(-1)]
@@ -22,14 +24,11 @@ lambda_range <- 10^seq(
 
 #Give number of folds
 k <- 5
-
-#Add column with fold-categories
-selection_col   <- sample(1:k,dim(X)[1][1],replace=TRUE)
-X_foldcat       <- cbind(X,selection_col)
-y_foldcat       <- cbind(y,selection_col)
+#init
+R_sq_lambda <- NULL
 
 #Loop over your lambda range
-for (m in lambda_range){
+for (lambda in lambda_range){
     
     #We use the Ridge-function, we have to store results in a matrix 
     
@@ -38,9 +37,12 @@ for (m in lambda_range){
     R_sq_holdout<- NULL
     
     #Start looping over all k folds, use k-fold- function
-    test <- k_fold_function(k,X_foldcat,y_foldcat,lambda_range)
-        
-        
+    R_sq <- k_fold_function(k,X,y,lambda)
+    R_sq_lambda<- rbind(R_sq_lambda,c(lambda,R_sq))
+}
 
-    }
+#find optimal lambda
 
+# do ridge for each lambda to get Beta for each lambda
+
+#plot 
--- Week 1 - Ridge Regression/K-fold function.R
+++ Week 1 - Ridge Regression/K-fold function.R
@@ -8,7 +8,7 @@ k_fold_function <- function(k,X,y,lambda){
 
 B <- NULL
 R_sq_holdout<- NULL
-y_est<- NULL
+y_centered_est<- NULL
 
 #Start looping over all k folds
 for (i in 1:k) {
@@ -20,24 +20,27 @@ for (i in 1:k) {
     
     X_train_scaled  <- scaling_function(X_train, TRUE, TRUE)
     X_holdout_scaled<- scaling_function(X_holdout, TRUE, TRUE)
+
+    y_train_centered  <- scaling_function(y_train, TRUE, FALSE)
     
     #Store estimates in coefficient matrix B
-    result          <- as.data.frame(ols_ridge_function(X_train_scaled,y_train,lambda,FALSE,FALSE))
+    result          <- as.data.frame(ols_ridge_function(X_train_scaled,y_train_centered,lambda,FALSE,FALSE))
     b_estimates     <- as.matrix(result$estimates)
     B               <- cbind(B,b_estimates)
     colnames(B)[ncol(B)] <- paste("Fold #",toString(i))
     #Estimate y_holdout
-    y_est_k         <- X_holdout_scaled %*% b_estimates
-    y_est           <- rbind(y_est,y_est_k)
+    y_centered_est_k<- X_holdout_scaled %*% b_estimates
+    y_centered_est  <- rbind(y_centered_est,y_centered_est_k)
     
 }
 
-error           <- y - y_est
+y_centered      <- scaling_function(y, TRUE, FALSE)
+error           <- y_centered - y_centered_est
 e_sq            <- t(error) %*% error
 SST             <- t(y - mean(y)) %*% (y - mean(y))   #no need to take the diff from mean, as this is set to zero
 R_sq            <- 1 - e_sq/SST
 
-return(y_est)
+return(y_centered_est)
 }
 
 
