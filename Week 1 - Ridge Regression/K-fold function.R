3f1ca5e5d0b4089dab2b7b56e4650eb7623ff9fc
Merlijn-van-Breugel <merlijnvanbreugel@gmail.com>
2016-09-05 14:45:48 +0200

    function for k-fold

---
+++ Week 1 - Ridge Regression/K-fold function.R
@@ -0,0 +1,51 @@
+
+# Title:    Week 1 Ridge Regression - K-fold function
+# Course:   Multivariate Statistics
+# Authors:  Merlijn van Breugel
+# Date:     05-09-2016
+
+k_fold_function <- function(k,X,y,lambda){
+
+B <- NULL
+R_sq_holdout<- NULL
+y_est<- NULL
+
+#Start looping over all k folds
+for (i in 1:k) {
+    
+    X_train         <- X_foldcat[X_foldcat[,dim(X_foldcat)[2][1]]!=i,][,1:ncol(X_foldcat)-1]
+    X_holdout       <- X_foldcat[X_foldcat[,dim(X_foldcat)[2][1]]==i,][,1:ncol(X_foldcat)-1]
+    y_train         <- cbind(y_foldcat[y_foldcat[,dim(y_foldcat)[2][1]]!=i,][,1:ncol(y_foldcat)-1])
+    y_holdout       <- cbind(y_foldcat[y_foldcat[,dim(y_foldcat)[2][1]]==i,][,1:ncol(y_foldcat)-1])
+    
+    X_train_scaled  <- scaling_function(X_train, TRUE, TRUE)
+    X_holdout_scaled<- scaling_function(X_holdout, TRUE, TRUE)
+    
+    #Store estimates in coefficient matrix B
+    result          <- as.data.frame(ols_ridge_function(X_train_scaled,y_train,lambda,FALSE,FALSE))
+    b_estimates     <- as.matrix(result$estimates)
+    B               <- cbind(B,b_estimates)
+    colnames(B)[ncol(B)] <- paste("Fold #",toString(i))
+    #Estimate y_holdout
+    y_est_k         <- X_holdout_scaled %*% b_estimates
+    y_est           <- rbind(y_est,y_est_k)
+    
+}
+
+error           <- y - y_est
+e_sq            <- t(error) %*% error
+SST             <- t(y - mean(y)) %*% (y - mean(y))   #no need to take the diff from mean, as this is set to zero
+R_sq            <- 1 - e_sq/SST
+
+return(y_est)
+}
+
+
+
+    
+    
+    
+ 
+
+
+
